128ea1fad5bdb735a9710cd5d1486680e8cdca83
Gabriel Montalvo <61370711+Tau-cloud@users.noreply.github.com>
2022-03-14 18:45:41 -0600

    Add files via upload

---
+++ ejercicio-css-Tau-cloud/14mar2022/scripts.js
@@ -0,0 +1,11 @@
+// console.log("hola")
+var head = document.getElementById("header")
+console.log(head)
+
+var parrafos = document.getElementsByTagName("p");
+console.log(parrafos);
+
+var p1 = document.getElementsByClassName("p1");
+console.log(p1);
+
+var p_X = document.querySelectorAll('[type="x"]');
